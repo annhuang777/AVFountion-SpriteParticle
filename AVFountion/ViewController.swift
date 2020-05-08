482606d3b0e6bb00cacfcdb0e11f275d7ce3c7be
annhuang <ann.huang777@gmail.com>
2020-05-08 20:07:19 +0800

    Initial Commit

---
+++ AVFountion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFountion
+//
+//  Created by Pin yu Huang on 2020/5/8.
+//  Copyright © 2020 Pin yu Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
